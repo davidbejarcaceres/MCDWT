134e76bbf582aaca65ac0d37c6c35f743aac264b
David Bejar Caceres <davidbejar2009@gmail.com>
2019-02-22 17:24:57 +0100

    Finall Script for issue 5
    Issue 5 solved and showed in class.
    Takes a sequence of sub-bands to apply quantification with k levels.

---
+++ tools/quant.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+
+# Note: swap the above line with the following two ones to switch
+# between the standard and the optimized running mode.
+
+#!/bin/sh
+''''exec python3 -O -- "$0" ${1+"$@"} # '''
+
+"""quantizer.py: Scalar Image quantizer for subbands in 16 bits using, also capable of using
+                 klustering Kmeans (sometimes better results with less levels using more CPU)
+                 Better for subbands."""
+
+import cv2
+import numpy as np
+import math
+import argparse
+import subprocess
+from cv2 import Sobel
+from skimage import data, draw, transform, util, color, filters
+import pylab
+from PIL import Image
+
+# Creates the command line arguments
+parser = argparse.ArgumentParser("Calculates gain of an image calculating energies\nMore info: https://docs.opencv.org/3.1.0/d1/d5c/tutorial_py_kmeans_opencv.html")
+parser.add_argument("-p" , help="Path to the MCDWT images: /tmp/video_transformed/16bits/", default = "/tmp/video_transformed/16bit/", type = str)
+parser.add_argument("-n", help="Number of images (NOT SUB-BANDS): 20 bands = 5 images", default = 5, type = int)
+parser.add_argument("-step", "--step", help="Quantization steps", default = 16, type = int)
+parser.add_argument("-kmeans", "--kmeans", help="Activates the advanced quantizer with Kmeans (+CPU ussage)\Sometimes better results using less steps, better for sub-bands", action='store_true')                                
+
+args = parser.parse_args() # Parses all the arguments
+print("Starts")
+K = args.step
+nImages = args.n
+path =  args.p
+print("Path:  "+path)
+bands = ["LL", "HL", "LH", "HH"]
+
+if (args.kmeans):
+    print("Using kmeans... ")
+    for image in range(nImages):
+        print("\nWorking in sub-bands from image: {:03d}".format(image))
+        for band in bands:
+            print("Band {}".format(band))
+            subprocess.run("python -O substract_offset.py -i {}{}{:03d}.png -o /tmp/normalized_kmeans.png".format(path, band , image), shell=True, check=True)
+            print("Offset Removed from the img to normalized")
+
+            img = cv2.imread("/tmp/normalized_kmeans.png", -1)
+            Z = img.reshape((-1,3))
+
+            # convert to np.float32
+            Z = np.float32(Z)
+            # define criteria, number of clusters(K) and apply kmeans()
+            criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+            ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+            
+            # Now convert back into uint8, and make original image
+            center = np.uint8(center)
+            res = center[label.flatten()]
+            res2 = res.reshape((img.shape))
+            if (band == "LL"):
+                cv2.imshow('Quantized Sub-band LL K = {}'.format(K),res2)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+            cv2.imwrite("/tmp/quantizedKmeans.png", res2.astype(np.uint16)) # Saves the image to tmp indicating the number of steps
+            subprocess.run("python3 -O add_offset.py -i /tmp/quantizedKmeans.png -o {}{}{:03d}.png".format(path, band , image), shell=True, check=True)
+            print("Offset Added, sub-band ready!")
+else:
+    for image in range(nImages):
+        print("\nWorking in sub-bands from image: {:03d}".format(image))
+        for band in bands:
+            print("Band {}".format(band))
+            subprocess.run("python -O substract_offset.py -i {}{}{:03d}.png -o /tmp/normalized.png".format(path, band , image), shell=True, check=True)
+            print("Offset Removed from the img to normalized")
+
+            # Implements an uniform scalar [quantizer]
+            imgTest = Image.open("/tmp/normalized.png")
+            im2Test = imgTest.quantize(K)
+            if (band == "LL"):
+                im2Test.show() # Displays the LL subbands
+            im2Test.save("/tmp/quantized.png","PNG")
+
+            # Reverts to 16 bits images to work with MCDWT project REPLACING THE ORIGINAL IMAGE
+            subprocess.run("python3 -O add_offset.py -i /tmp/quantized.png -o {}{}{:03d}.png".format(path, band , image), shell=True, check=True)
+            print("Offset Added, sub-band ready!")
+        
+print("DONE!")    
