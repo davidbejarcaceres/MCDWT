1dd9cc74d85a1e2c0fd1cf40ed6ab65891ed6a16
davidbejarcaceres <davidbejar2009@gmail.com>
2020-02-06 13:15:55 +0100

    CUDA implemented into MCDWT

--- src/MC/optical/motion.py
+++ src/MC/optical/motion.py
@@ -13,7 +13,7 @@ except:
 
 if cuda_enabled:
     try:
-        blank_image = numpy.zeros((500,500,3), numpy.uint8)
+        blank_image = np.zeros((500,500,3), np.uint8)
         blank_image.fill(200)
         nvof = cv2.cuda_NvidiaOpticalFlow_1_0.create(blank_image.shape[1], blank_image.shape[0], 5, False, False, False, 1)
         flow = nvof.calc(cv2.cvtColor(blank_image, cv2.COLOR_BGR2GRAY ), cv2.cvtColor(blank_image, cv2.COLOR_BGR2GRAY ), None)
@@ -21,6 +21,8 @@ if cuda_enabled:
     except:
         print("No CUDA Turing GPU")
     pass
+
+
 
 
 def generate_prediction(curr, next, base):
@@ -43,11 +45,24 @@ def estimate_frame(base, flow):
             interpolation=cv2.INTER_LINEAR,
             borderMode=cv2.BORDER_REPLICATE)
 
-def opticalFlowCuda(imgPrev: np.uint8, gray: np.uint8):
+def opticalFlowCuda(next_y: np.uint8, curr_y: np.uint8) -> cv2.UMat:
+    if cuda_turing_aceleration_sdk:
+        return opticalFlowCuda_testla(next_y, curr_y)
     ############  CUDA Optical Flow ###############
-    g_prev_gpu: cv2.cuda_GpuMat = cv2.cuda_GpuMat(imgPrev) # Uploads image to GPU
-    g_next_gpu: cv2.cuda_GpuMat = cv2.cuda_GpuMat(gray) # Uploads image to GPU
-    flowGPU = opticalFlowGPUCalculator.calc(g_prev_gpu, g_next_gpu, None)  # Calculate on GPU
-    flow = flowGPU.download() # Copies the optical flow from GPU to Host
+    next_y_gpu: cv2.cuda_GpuMat = cv2.cuda_GpuMat(next_y) # Uploads image to GPU
+    curr_y_gpu: cv2.cuda_GpuMat = cv2.cuda_GpuMat(curr_y) # Uploads image to GPU
+    flowGPU: cv2.cuda_GpuMat = opticalFlowGPUCalculator.calc(next_y_gpu, curr_y_gpu, None)  # Calculate on GPU
+    flow:  cv2.UMat = flowGPU.download() # Copies the optical flow from GPU to Host
     ###############################################
     return flow
+
+
+def opticalFlowCuda_testla(next_y: np.uint8, curr_y: np.uint8) -> cv2.UMat:
+    nvof = cv2.cuda_NvidiaOpticalFlow_1_0.create(next_y.shape[1], next_y.shape[0], 5, False, False, False, 1)
+    flow:  cv2.UMat = nvof.calc(next_y, curr_y, None)
+    return flow;
+
+blank_image = np.zeros((500,500,3), np.uint8)
+blank_image.fill(200)
+
+generate_prediction(blank_image, blank_image, blank_image)
